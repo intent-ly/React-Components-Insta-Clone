5961e30138e836fafaee16e8efd6ccc3beca3b2a
intent-ly <erick.skillsusa@gmail.com>
2020-10-06 15:50:30 -0700

    version1

--- src/App.js
+++ src/App.js
@@ -5,15 +5,21 @@
 */
 
 // Import the state hook
-import React from 'react';
+import React, {useState} from 'react';
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
 // Import the dummyData
 import './App.css';
+import Post from './components/Posts/Post';
+import Posts from './components/Posts/Posts'
+import SearchBar from './components/SearchBar/SearchBar'
+import dummyData from './dummy-data'
 
 const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
+
+  const [posts , setPosts] = useState(dummyData)
 
   const likePost = postId => {
     /*
@@ -21,18 +27,30 @@ const App = () => {
 
       The state of the app lives at the top of the React tree, but it wouldn't be fair for nested components not to be able to change state!
       This function is passed down to nested components through props, allowing them to increase the number of likes of a given post.
-
+      
       Invoke `setPosts` and pass as the new state the invocation of `posts.map`.
       The callback passed into `map` performs the following logic:
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
-  };
+   setPosts (
+     posts.map(stuff=>{
+        if (stuff.id == postId){
+          return {...stuff, likes:stuff.likes + 1}
+        }
+        else{
+          return stuff
+        }
+     })
+   )}
 
   return (
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
+      <SearchBar/>
+      <Posts posts ={posts} likePost = {likePost}/>
       {/* Check the implementation of each component, to see what props they require, if any! */}
+
     </div>
   );
 };
